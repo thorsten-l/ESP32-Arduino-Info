4dc6705fc7cf2b3db2ad911f5a012728fc627114
Thorsten Ludewig <th@l9g.de>
2019-04-02 10:43:17 +0200

    wifi autoreconnect implemented

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,39 @@ void secondTask( void * parameter )
   vTaskDelete(NULL);
 }
 
+void connectWiFi()
+{
+  Serial.print("\nConnecting to WiFi network ");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.persistent(true);
+  WiFi.setHostname( OTA_HOSTNAME );
+  WiFi.begin( WIFI_SSID, WIFI_PASS );
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    digitalWrite( BOARD_LED, 1 ^ digitalRead(BOARD_LED));
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println(" connected.\n");
+  digitalWrite( BOARD_LED, false );
+
+  WiFi.macAddress(mac);
+  Serial.printf("WiFi MAC Address    : %02X:%02X:%02X:%02X:%02X:%02X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5] );
+  Serial.printf("WiFi Hostname       : %s\n", WiFi.getHostname());
+  Serial.print( "WiFi IP-Address     : " );
+  Serial.println( WiFi.localIP() );
+  Serial.print( "WiFi Gateway-IP     : " );
+  Serial.println( WiFi.gatewayIP() );
+  Serial.print( "WiFi Subnetmask     : " );
+  Serial.println( WiFi.subnetMask() );
+  Serial.print( "WiFi DNS Server     : " );
+  Serial.println( WiFi.dnsIP() );
+  Serial.println();
+}
+
 void setup()
 {
   pinMode( BOARD_LED, OUTPUT );
@@ -67,43 +100,12 @@ void setup()
   Serial.printf("Running core        : %d\n", xPortGetCoreID() );
   xSemaphoreGive( mutex );
 
-  Serial.println();
-
   WiFi.disconnect();
   WiFi.persistent(false);
   WiFi.mode(WIFI_OFF);
+  delay(1000);
 
-  Serial.print("Connecting to WiFi network ");
-  delay(500);
-
-  WiFi.begin();
-  WiFi.setHostname( OTA_HOSTNAME );
-  WiFi.begin( WIFI_SSID, WIFI_PASS );
-  WiFi.setAutoConnect(true);
-  WiFi.setAutoReconnect(true);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    digitalWrite( BOARD_LED, 1 ^ digitalRead(BOARD_LED));
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println(" connected.\n");
-  digitalWrite( BOARD_LED, false );
-
-  WiFi.macAddress(mac);
-  Serial.printf("WiFi MAC Address    : %02X:%02X:%02X:%02X:%02X:%02X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5] );
-  Serial.printf("WiFi Hostname       : %s\n", WiFi.getHostname());
-  Serial.print( "WiFi IP-Address     : " );
-  Serial.println( WiFi.localIP() );
-  Serial.print( "WiFi Gateway-IP     : " );
-  Serial.println( WiFi.gatewayIP() );
-  Serial.print( "WiFi Subnetmask     : " );
-  Serial.println( WiFi.subnetMask() );
-  Serial.print( "WiFi DNS Server     : " );
-  Serial.println( WiFi.dnsIP() );
-
+  connectWiFi();
   InitializeOTA();
 
   Serial.println();
@@ -135,11 +137,18 @@ void loop()
 
   if (( currentTimestamp - lastTimestamp >= 1000 ))
   {
+    bool wifiIsConnected = WiFi.isConnected();
     Serial.printf("\r[%d] Running: ", xPortGetCoreID() );
     Serial.print( rollingChars[counter]);
+    Serial.printf( " and WiFi is%s connected", wifiIsConnected ? "" : " NOT");
     counter++;
     counter %=4 ;
     lastTimestamp = currentTimestamp;
+
+    if( wifiIsConnected == false )
+    {
+      connectWiFi();
+    }
   }
 
   ArduinoOTA.handle();
