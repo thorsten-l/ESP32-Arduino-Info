76a471b08f90bbb9bec8314f83a80c7c54f8c8b7
Thorsten Ludewig <th@l9g.de>
2019-04-19 20:16:44 +0200

    Build project with Docker

---
+++ docker/BUILD.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -f firmware.*
+docker build -t pio-esp32-adruino-info .
+docker run --name pio-esp32-adruino-info pio-esp32-adruino-info
+docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pioenvs/lolin32/firmware.bin firmware.lolin32
+docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pioenvs/lolin32lite/firmware.bin firmware.lolin32lite
+docker rm pio-esp32-adruino-info
