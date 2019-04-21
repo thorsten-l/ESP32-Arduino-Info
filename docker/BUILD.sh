8525af77ca875271cca7b8acaba8519e2c79c29a
Thorsten Ludewig <th@l9g.de>
2019-04-21 15:19:01 +0200

    esp32cam added to docker build process

--- docker/BUILD.sh
+++ docker/BUILD.sh
@@ -4,4 +4,5 @@ docker build -t pio-esp32-adruino-info .
 docker run --name pio-esp32-adruino-info pio-esp32-adruino-info
 docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pioenvs/lolin32/firmware.bin firmware.lolin32
 docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pioenvs/lolin32lite/firmware.bin firmware.lolin32lite
+docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pioenvs/esp32cam/firmware.bin firmware.esp32cam
 docker rm pio-esp32-adruino-info
--- docker/cmd.sh
+++ docker/cmd.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/thorsten-l/ESP32-Arduino-Info.git
 cd /ESP32-Arduino-Info
 cp lib/App/App.hpp.sample lib/App/App.hpp
-/usr/local/bin/platformio run -e lolin32 -e lolin32lite
+/usr/local/bin/platformio run -e lolin32 -e lolin32lite -e esp32cam
