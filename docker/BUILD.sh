3445a8cb1649eb2a129d5fe2fcca2350cb4666d9
Thorsten Ludewig <th@l9g.de>
2019-10-09 21:50:29 +0200

    change to platformio version 4.0

--- docker/BUILD.sh
+++ docker/BUILD.sh
@@ -2,7 +2,7 @@
 rm -f firmware.*
 docker build -t pio-esp32-adruino-info .
 docker run --name pio-esp32-adruino-info pio-esp32-adruino-info
-docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pioenvs/lolin32/firmware.bin firmware.lolin32
-docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pioenvs/lolin32lite/firmware.bin firmware.lolin32lite
-docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pioenvs/esp32cam/firmware.bin firmware.esp32cam
+docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pio/build/lolin32/firmware.bin firmware.lolin32
+docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pio/build/lolin32lite/firmware.bin firmware.lolin32lite
+docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pio/build/esp32cam/firmware.bin firmware.esp32cam
 docker rm pio-esp32-adruino-info
