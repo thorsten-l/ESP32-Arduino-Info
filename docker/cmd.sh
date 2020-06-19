e39bacc7258f3592caf20d8bbfa78ba60950838e
Thorsten Ludewig <th@l9g.de>
2020-06-19 15:56:07 +0200

    App.hpp.sample not longe used

--- docker/cmd.sh
+++ docker/cmd.sh
@@ -2,5 +2,10 @@
 
 git clone https://github.com/thorsten-l/ESP32-Arduino-Info.git
 cd /ESP32-Arduino-Info
-cp lib/App/App.hpp.sample lib/App/App.hpp
-/usr/local/bin/platformio run -e esp32-poe -e lolin32 -e lolin32lite
+/usr/local/bin/platformio run \
+  -e esp32-poe \
+  -e esp32cam \
+  -e heltec_wifi_lora_32 \
+  -e esp32dev \
+  -e lolin32 \
+  -e lolin32lite
