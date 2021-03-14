dbcd551e04b7a662006e76ec9daa9e44a1b08859
Thorsten Ludewig <th@l9g.de>
2021-03-14 16:45:41 +0100

    docker build environment updated

--- docker/BUILD.sh
+++ docker/BUILD.sh
@@ -9,4 +9,7 @@ docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pio/build/esp32cam/firmwar
 docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pio/build/esp32-poe/firmware.bin firmware.esp32-poe
 docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pio/build/heltec_wifi_lora_32/firmware.bin firmware.heltec_wifi_lora_32
 docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pio/build/esp32dev/firmware.bin firmware.esp32dev
+docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pio/build/lilygo-t-eth-poe/firmware.bin firmware.lilygo-t-eth-poe
+docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pio/build/wt32-eth01/firmware.bin firmware.wt32-eth01
+docker cp pio-esp32-adruino-info:/ESP32-Arduino-Info/.pio/build/wemos_d1_mini32/firmware.bin firmware.wemos_d1_mini32
 docker rm pio-esp32-adruino-info
--- docker/cmd.sh
+++ docker/cmd.sh
@@ -4,6 +4,9 @@ git clone https://github.com/thorsten-l/ESP32-Arduino-Info.git
 cd /ESP32-Arduino-Info
 /usr/local/bin/platformio run \
   -e esp32-poe \
+  -e lilygo-t-eth-poe \
+  -e wt32-eth01 \
+  -e wemos_d1_mini32 \
   -e esp32cam \
   -e heltec_wifi_lora_32 \
   -e esp32dev \
